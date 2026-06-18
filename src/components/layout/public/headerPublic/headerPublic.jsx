4c54e916866eb9cbe2c49307dd6b748189e97c96
fateme <fatemesofyan1576@@gmail.com>
2026-06-19 01:33:16 +0330

    setup project structure (folders and architecture)

---
+++ src/components/layout/public/headerPublic/headerPublic.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+export default function HeaderPublic() {
+  return (
+      <header className=" bg-blue-900 shadow-xl">
+        <h1> قیمت یار</h1>
+      {/* <HeaderTop />
+      <Navbar />
+      <HeaderActions /> */}
+    </header>
+  )
+}
