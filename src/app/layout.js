7856fbe296d79fe11599abbd229fb6869fea879d
fateme <fatemesofyan1576@@gmail.com>
2026-06-21 18:54:09 +0330

    resolve layout conflict

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import HeaderPublic from "../components/layout/public/headerPublic/headerPublic";
+import FooterPublic from "../components/layout/public/footerPublic/FooterPublic";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,7 +25,11 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <HeaderPublic />
+        {children}
+        <FooterPublic />
+      </body>
     </html>
   );
 }
